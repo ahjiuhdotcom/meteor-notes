601df2ef09c3a68bfc666e04b19348ed53230bc3
ahjiuhdotcom <ahjiuhdotcom@gmail.com>
2017-05-18 16:58:52 +0800

    Select newly created note. Sort notes by updatedAt

--- imports/ui/NoteList.js
+++ imports/ui/NoteList.js
@@ -33,7 +33,11 @@ export default createContainer(() => {
 
   return {
     // find all the notes in the subscriptions
-    notes: Notes.find().fetch().map((note) => {
+    notes: Notes.find({}, {
+      sort: {
+        updatedAt: -1
+      }
+    }).fetch().map((note) => {
       return {
         ...note,
         selected: note._id === selectedNoteId // return true or false here
